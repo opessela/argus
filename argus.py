d355d421b7ce9a8d76249814da11366733652c7a
kecorbin <kecorbin@cisco.com>
2017-11-09 01:53:41 -0600

    implement ManagedUCS object class

--- argus.py
+++ argus.py
@@ -4,63 +4,76 @@ import requests
 import datetime
 from acitoolkit import Session, Credentials
 from ucsmsdk.ucshandle import UcsHandle
-from ucs import provision_ucs_pod, deprovision_ucs_pod, get_vlan_id_by_name
+from ucs import ManagedUCS # provision_ucs_pod, deprovision_ucs_pod, get_vlan_id_by_name
 from aci import VlanBinding, ManagedTopology, PortGroup, fixup_epg_name, get_bindings_for_lsnode
 from utils import run_async
+import yaml
 
 import config
 
 HEADERS = {"Content-Type": "application/json"}
 
-def perform_garbage_collection():
-    """
-    periodically ran to ensure that no unused vlans are provisioned on UCS domains
-    Returns:
+def full_sync():
 
-    """
-    print("Starting Garbage Collection...")
-    print("Gathering DVS information")
-    port_groups = PortGroup.get(apic)
-    port_groups_by_vlan = dict()
-    port_groups_by_name = dict()
-    for item in port_groups:
-        port_groups_by_name[item.name.replace('|', config.DELIMTER)] = item.vlan
-        port_groups_by_vlan[item.vlan] = item.name.replace('|', config.DELIMTER)
+    print("Validating existing ACI bindings are provisioned in UCS")
 
-    print("Retrieving UCS configuration")
-    provisioned_vlans = dict()
-    for vip in config.UCS.keys():
+    existing_bindings = VlanBinding.get(apic, topology=topology, vip_map=config.UCSM_VIP_MAP)
+    for ucs in config.UCS.keys():
+        print "Synchronizing Existing bindings for UCS pod {}".format(ucs)
+        ucsm = ManagedUCS(ucs, config.UCSM_LOGIN, config.UCSM_PASSWORD)
+        ucsm.login()
+        bindings = [b for b in existing_bindings if b.ucsm == ucs]
 
-        handle = UcsHandle(vip, config.UCSM_LOGIN, config.UCSM_PASSWORD)
-        handle.login()
-        members = handle.query_children(in_dn=config.VLAN_GROUP_DN, class_id='FabricPooledVlan')
-        epgs = [vlan.name for vlan in members]
-        provisioned_vlans[vip] = epgs
+        # all ACI vlans which are provisioned
+        aci_vlans = set([b.vlan for b in bindings])
 
-        fis = config.UCS[vip]
-        vlans = epgs
-        for fi_mgmt_ip in fis.values():
-            bindings_for_fi = get_bindings_for_lsnode(apic, fi_mgmt_ip)
-            print("Reconciling vlans {} with {}".format(vlans, bindings_for_fi))
-            for vlan in vlans:
-                print vlan
-                vlan_dn = fixup_epg_name(vlan[-32:])
+        # all vlans provisioned on this UCS pod
+        ucs_vlans = set([v.id for v in ucsm.get_vlans()])
 
-                if vlan_dn is not None:
+        # we are only concerned about ucs_vlans which are part of the dvs
+        ucs_vlans = ucs_vlans.intersection(dvs_vlans)
 
-                    is_valid = False
-                    for b in bindings_for_fi:
-                        if vlan_dn in b['fvDyPathAtt']['attributes']['dn']:
-                            is_valid = True
-                    if is_valid:
-                        print "VLAN {} has active bindings, leaving it alone".format(vlan)
-                    else:
-                        vlan_id = get_vlan_id_by_name(handle, vlan)
-                        print "We should probably delete vlan {} encap {}".format(vlan, vlan_id)
-                        deprovision_ucs_pod(handle, vlan, vlan_id)
+        # vlans that exist in ACI but not in UCS and must be provisioned
+        missing_vlans = aci_vlans.difference(ucs_vlans)
+        print "Need to provision {}".format(missing_vlans)
+        for v in missing_vlans:
+            epg = port_groups_by_vlan[v]
+            print("Adding {}".format(epg))
+            ucsm.provision_portgroup(epg, v)
 
-        handle.logout()
-        print("Garbage Collection Completed")
+        # verify that all aci_vlans exist in the vlan group
+        vlan_group_members = ucsm.get_vlan_group_members()
+        vlan_group_members = set([v.name for v in vlan_group_members])
+
+        print("VLAN group members {}".format(vlan_group_members))
+        expected_group_members = set(map(lambda x : ucsm.ucs_name_from_portgroup(x),
+                                     [port_groups_by_vlan[v] for v in aci_vlans]))
+        print("Expected Group members {}".format(expected_group_members))
+        print port_groups_by_name.keys()
+        if not expected_group_members == vlan_group_members:
+            print("Group Membership is out of sync")
+            missing_group_members = expected_group_members.difference(vlan_group_members)
+            print("Missing Group Members {}".format(missing_group_members))
+            for pg in missing_group_members:
+                ucsm.add_vlan_to_group(ManagedUCS.ucs_name_from_portgroup(pg))
+            extra_group_members = vlan_group_members.difference(expected_group_members)
+            extra_group_members = [pg for pg in extra_group_members if pg in port_groups_by_name.keys()]
+            print("Extra Group Members {}".format(extra_group_members))
+            for member in extra_group_members:
+                ucsm.remove_vlan_from_group(member)
+
+        # vlans that exist in ucs but not in ACI
+        extra_vlans = ucs_vlans.difference(aci_vlans)
+        # vlan id 1 cannot be removed from UCS
+        extra_vlans.discard('1')
+        print "Need to de-provision {}".format(extra_vlans)
+        for v in extra_vlans:
+            epg = port_groups_by_vlan[v]
+            print("Removing {}".format(epg))
+            ucsm.deprovision_portgroup(epg, v)
+
+        # logout
+        ucsm.logout()
 
 
 @run_async
@@ -76,19 +89,20 @@ def binding_event_handler(session, epg, binding):
         if ucsm_ip:
             print("Found UCSM on {}".format(ucsm_ip))
             send_event(binding.status, epg.name, binding.node, binding.port, binding.vlan, ucsm_ip)
-            handle = UcsHandle(ucsm_ip, config.UCSM_LOGIN, config.UCSM_PASSWORD)
-            handle.login()
+            ucsm = ManagedUCS(ucsm_ip, config.UCSM_LOGIN, config.UCSM_PASSWORD)
+            ucsm.login()
 
             if binding.status == 'created':
                 print("Adding {} to {}".format(epg.name, ucsm_ip))
-                provision_ucs_pod(handle, epg.name, binding.vlan)
+                ucsm.provision_portgroup(epg.name, binding.vlan)
             elif binding.status == 'deleted':
                 print("Removing {} from {}".format(epg.name, ucsm_ip))
-                deprovision_ucs_pod(handle, epg.name, binding.vlan)
+                ucsm.deprovision_portgroup(epg.name, binding.vlan)
+                #deprovision_ucs_pod(handle, epg.name, binding.vlan)
             else:
                 print("Existing Binding - Verifying {} has {} provisoned".format(ucsm_ip, binding.vlan))
-                provision_ucs_pod(handle, epg.name, binding.vlan)
-            handle.logout()
+                #provision_ucs_pod(handle, epg.name, binding.vlan)
+            ucsm.logout()
 
         else:
             print("None Found")
@@ -111,6 +125,7 @@ def send_event(action, epg, node, port, vlan, ucsm):
 
 
 if __name__ == "__main__":
+
     description = 'Argus'
 
     # Gather credentials for ACI toolkit
@@ -122,18 +137,24 @@ if __name__ == "__main__":
 
     if apic.login().ok:
         print("Connected to ACI")
+    print("Collecting Topology Information")
     topology = ManagedTopology.get(apic, config.UCSM_VIP_MAP)
 
-    existing_bindings = VlanBinding.get(apic)
-    print("Validating existing ACI bindings are provisioned in UCS")
+    print("Gathering DVS information")
+    # these objects are referenced to ensure we only operate against DVS vlans/portgroups
+    port_groups = PortGroup.get(apic)
+    port_groups_by_vlan = dict()
+    port_groups_by_name = dict()
+    for item in port_groups:
+        # normalize portgroup name
+        item.name = ManagedUCS.ucs_name_from_portgroup(item.name)
+        port_groups_by_name[item.name] = item.vlan
+        port_groups_by_vlan[item.vlan] = item.name
+    dvs_vlans = set([v.vlan for v in port_groups])
 
-    print existing_bindings
-    for b in existing_bindings:
-        epg = PortGroup.get(apic, encap=b.vlan)
-        binding_event_handler(apic, epg, b)
+    full_sync()
 
-    # perform_garbage_collection()
-    # last_garbage_collection = datetime.datetime.now()
+    last_full_sync = datetime.datetime.now()
 
     print("Creating subscription to ACI fabric")
     print("=" * 80)
@@ -149,11 +170,12 @@ if __name__ == "__main__":
             if not already_provsioned:
                 binding_event_handler(apic, epg, binding)
 
-        # Run garbage collection periodically
-        # if config.GARBAGE_COLLECTION_INTERVAL:
-        #     if config.GARBAGE_COLLECTION_INTERVAL < 15:
-        #         config.GARBAGE_COLLECTION_INTERVAL = 15
-        #     delta = datetime.datetime.now() - last_garbage_collection
-        #     if delta > datetime.timedelta(minutes=config.GARBAGE_COLLECTION_INTERVAL):
-        #         perform_garbage_collection()
-        #         last_garbage_collection = datetime.datetime.now()
+        #Run garbage collection periodically
+        if config.GARBAGE_COLLECTION_INTERVAL:
+            # if config.GARBAGE_COLLECTION_INTERVAL < 15:
+            #     config.GARBAGE_COLLECTION_INTERVAL = 15
+            #
+            delta = datetime.datetime.now() - last_full_sync
+            if delta > datetime.timedelta(minutes=config.GARBAGE_COLLECTION_INTERVAL):
+                full_sync()
+                last_full_sync = datetime.datetime.now()
--- ucs.py
+++ ucs.py
@@ -1,8 +1,185 @@
+from ucsmsdk.ucshandle import UcsHandle
 from ucsmsdk.mometa.fabric.FabricVlan import FabricVlan
 from ucsmsdk.mometa.fabric.FabricPooledVlan import FabricPooledVlan
+from ucsmsdk.mometa.network import NetworkElement
 from ucsmsdk.ucsexception import UcsException
+
 import config
 
+
+class ManagedUCS(UcsHandle):
+    """
+    A managed UCS system
+    """
+    def __init__(self, ip, username, password, port=None, secure=None,
+                 proxy=None):
+        self._fabric_interconnects = list()
+        self._vlans = list()
+        self._managed_vlan_group = config.DVS_NAME
+        super(ManagedUCS, self).__init__(ip, username, password, port=port, secure=secure,
+                                         proxy=proxy)
+
+    @staticmethod
+    def ucs_name_from_portgroup(name, aci_delimter='|', ucs_delimter=config.DELIMTER):
+        """
+        sanitize portgroup name for ucs provisioning
+        Args:
+            name: name of portgroup
+        Returns:
+
+        """
+        return name.replace(aci_delimter, ucs_delimter)[-30:]
+
+    @property
+    def vlans(self):
+        return self._vlans
+
+    def get_fabric_interconnects(self):
+        fis = self.query_classid('networkElement')
+        self._fabric_interconnects = fis
+        return self._fabric_interconnects
+
+    def get_neighbors(self, ):
+        neighbors = self.query_classid('networkLanNeighborEntry')
+        return neighbors
+
+    def get_vlans(self):
+        vlans = self.query_classid('FabricVlan')
+
+        self._vlans = vlans
+        return self._vlans
+
+    def get_vlan_group(self):
+        """
+        returns the managed vlan group object
+        Returns:
+
+        """
+        vlan_group = self.query_dn('fabric/lan/net-group-{}'.format(self._managed_vlan_group))
+        return vlan_group
+
+    def get_vlan_group_members(self):
+        mo = self.get_vlan_group()
+        vlans = self.query_children(in_dn=mo.dn, class_id='fabricPooledVlan')
+        return vlans
+
+    def add_vlan(self, vlan_id=None, vlan_name=None):
+        """
+        adds a vlan to the managed ucs system.
+        """
+        mo = FabricVlan(parent_mo_or_dn="fabric/lan",
+                        sharing="none",
+                        name=vlan_name, id=vlan_id)
+        exists = self.query_dn(mo.dn)
+
+        if exists:
+            return exists
+        else:
+            try:
+                self.add_mo(mo)
+                self.commit()
+                self._vlans.append(mo)
+                obj = self.query_dn(mo.dn)
+                return obj
+            except UcsException as e:
+                if 'already exists' in str(e):
+                    pass
+                else:
+                    raise e
+            return mo
+
+    def remove_vlan(self, vlan_object):
+        """
+        adds a vlan to the managed ucs system.
+        """
+        self.remove_mo(vlan_object)
+        self.commit()
+        return True
+
+    def add_vlan_to_group(self, name, vlan_group=config.VLAN_GROUP_DN):
+        # Add vlan to pool
+        add_to_pool = FabricPooledVlan(vlan_group, name)
+        mo = self.query_dn(add_to_pool.dn)
+        if mo is None:
+            try:
+                self.add_mo(add_to_pool)
+                self.commit()
+                return True
+            except UcsException as e:
+                if 'already exists' in str(e):
+                    return False
+                else:
+                    raise e
+
+    def remove_vlan_from_group(self, name):
+
+        dn = config.VLAN_GROUP_DN + '/net-{}'.format(name)
+        mo = self.query_dn(dn)
+        if mo:
+            self.remove_mo(mo)
+            self.commit()
+            return True
+
+    def provision_portgroup(self, name, vlan_id, vlan_group=config.DVS_NAME):
+
+        """
+        provisions a portgroup into ucs
+
+        1. sanitizes name
+        2. provisions vlan globally
+        3. add's vlan to group
+        Args:
+            portgroup_vlan_tuple: tuple (portgroup_name, vlan_id)
+
+        Returns: bool
+
+        """
+        try:
+            name = self.ucs_name_from_portgroup(name)
+            vlan = self.add_vlan(vlan_id=vlan_id, vlan_name=name)
+            self.add_vlan_to_group(vlan.name)
+            return True
+        except UcsException as e:
+            if 'already exists' in str(e):
+                return True
+            else:
+                raise e
+
+    def deprovision_portgroup(self, name, vlan_id, vlan_group=config.DVS_NAME):
+
+        """
+        removes a portgroups configuration from ucs
+
+        1. sanitizes name
+        2. removes vlan from group
+        3. removes vlan globally
+        Args:
+            portgroup_vlan_tuple: tuple (portgroup_name, vlan_id)
+
+        Returns: bool
+
+        """
+        try:
+
+            name = self.ucs_name_from_portgroup(name)
+            print name
+            vlan = FabricVlan(parent_mo_or_dn="fabric/lan",
+                sharing="none",
+                name=name, id=vlan_id)
+            self.remove_vlan_from_group(vlan.name)
+            self.remove_vlan(vlan)
+            return True
+
+        except UcsException as e:
+            if 'already exists' in str(e):
+                return False
+            else:
+                raise e
+
+
+    @property
+    def fi_ips(self):
+        return [ip.oob_if_ip for ip in self._fabric_interconnects]
 
 def add_vlan(handle, vlan_id, name):
     """
